47aa5296cb73efef78537be0a98217a8ba680203
Chris <47066922+cwilso28@users.noreply.github.com>
2025-08-19 22:08:10 -0500

    Create method to get the number of images
    The JS script will be used to programmatically create the navigation
    buttons in the future. This code is the start of determining how many
    images are in the carousel.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import "./styles.css"
+
+let ordered_list = document.getElementById("carousel-container");
+let line_items = ordered_list.getElementsByTagName("li");
+console.log(line_items.length);
