e95912c9a21906b38fa4808bc5570c8fd037f8b9
Chris <47066922+cwilso28@users.noreply.github.com>
2025-08-25 22:25:39 -0500

    Add comments for future code

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import "./styles.css"
 let ordered_list = document.getElementById("carousel-container");
 let line_items = ordered_list.getElementsByTagName("li");
 console.log(line_items.length);
+
+// Image carousel status
+
+// Adjust highlighted button as image carousel rotates or is rotated
+
+// 
