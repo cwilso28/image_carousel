4d99487dbec62ee3512d8cc3f6d1412a0dfcc2bb
Chris <47066922+cwilso28@users.noreply.github.com>
2025-08-26 21:32:26 -0500

    Add function to left and right arrow buttons
    The user needs a way to cycle through the images manually. Adding
    event listeners to the left and right arrows lets the user click through
    the images, but doesn't let the user go beyond the ends of the carousel.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,49 @@
 import "./styles.css"
 
-let ordered_list = document.getElementById("carousel-container");
-let line_items = ordered_list.getElementsByTagName("li");
-console.log(line_items.length);
-
 // Image carousel status
+class carouselManager {
+    constructor() {
+        this.setButtonFunction()
+        this.ordered_list = document.getElementById("carousel-container");
+        this.line_items = this.ordered_list.getElementsByTagName("li");
+        this.line_item_count = this.line_items.length;
+        this.image_index = 0;
+        this.position = 0;
+        console.log(this.line_items)
+    };
 
+    carouselForward() {
+        if (this.image_index < this.line_item_count - 1) {
+            this.image_index += 1;
+            this.position -= 100;
+
+            this.ordered_list.style.left = this.position + "%"
+        };
+    }
+
+    carouselBackward() {
+        if (this.image_index > 0) {
+            this.image_index -= 1;
+            this.position += 100;
+
+            this.ordered_list.style.left = this.position + "%"
+        }
+    }
+
+    setButtonFunction() {
+        let right_arrow = document.getElementById("right-arrow");
+        right_arrow.addEventListener("click", (e) => {
+            this.carouselForward();
+        })
+
+        let left_arrow = document.getElementById("left-arrow");
+        left_arrow.addEventListener("click", (e) => {
+            this.carouselBackward();
+        })
+    }
+}
 // Adjust highlighted button as image carousel rotates or is rotated
 
-// 
+//
+
+new carouselManager();
