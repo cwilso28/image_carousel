89b5ecd07bb887be132b47f0ee3676194ae9e342
Chris <47066922+cwilso28@users.noreply.github.com>
2025-08-27 21:39:58 -0500

    Add navigation function to carousel dots
    The carousel dots should advance the carousel the corresponding image to
    the button. The added code accomplishes this.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import "./styles.css"
 // Image carousel status
 class carouselManager {
     constructor() {
-        this.setButtonFunction()
+        this.setButtonFunction();
+        this.setCarouselNavigatorFunction();
         this.ordered_list = document.getElementById("carousel-container");
         this.line_items = this.ordered_list.getElementsByTagName("li");
         this.line_item_count = this.line_items.length;
@@ -17,7 +18,7 @@ class carouselManager {
             this.image_index += 1;
             this.position -= 100;
 
-            this.ordered_list.style.left = this.position + "%"
+            this.ordered_list.style.left = this.position + "%";
         };
     }
 
@@ -26,8 +27,15 @@ class carouselManager {
             this.image_index -= 1;
             this.position += 100;
 
-            this.ordered_list.style.left = this.position + "%"
+            this.ordered_list.style.left = this.position + "%";
         }
+    }
+
+    carouselAbsolute(index) {
+        this.image_index = index;
+        this.position = index * -100;
+
+        this.ordered_list.style.left = this.position + "%";
     }
 
     setButtonFunction() {
@@ -40,6 +48,20 @@ class carouselManager {
         left_arrow.addEventListener("click", (e) => {
             this.carouselBackward();
         })
+    }
+
+    parseRadioName(name) {
+        let name_components = name.split("_");
+        return name_components[1];
+    }
+
+    setCarouselNavigatorFunction() {
+        let carousel_navigator = document.getElementById("radio-buttons");
+        carousel_navigator.addEventListener("click", (e) => {
+            let id_check = document.querySelector('input[name="carousel"]:checked').id;
+            let index = this.parseRadioName(id_check) - 1;
+            this.carouselAbsolute(index);
+        });
     }
 }
 // Adjust highlighted button as image carousel rotates or is rotated
